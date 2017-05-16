67106a437cd56d9d7b91f022ff104d94c89ca8b9
Colin <smithcolina@hotmail.com>
2017-05-16 09:55:00 +0100

    Add unit tests

--- src/test/java/com/qmetric/supermarket/model/MoneyTest.java
+++ src/test/java/com/qmetric/supermarket/model/MoneyTest.java
@@ -14,4 +14,24 @@ public class MoneyTest {
 		assertThat(Money.of("12.00"), is(new Money(new BigDecimal("12.00"))));
 	}
 
+	@Test
+	public void testAdd() {
+		assertThat(Money.of("1.00").add(Money.of("1")), is(Money.of("2.00")));
+	}
+
+	@Test
+	public void testMultiplyInt() {
+		assertThat(Money.of("10").multiply(10), is(Money.of("100.00")));
+	}
+
+	@Test
+	public void testMultiplyBigDecimal() {
+		assertThat(Money.of("10").multiply(BigDecimal.TEN), is(Money.of("100.00")));
+	}
+
+	@Test
+	public void testMultiplyWeight() {
+		assertThat(Money.of("10").multiply(Weight.of("0.1")), is(Money.of("1.00")));
+	}
+
 }
